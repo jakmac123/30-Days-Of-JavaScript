4af58d9d2c6a0c4314c51bc4f8119a91d8b471e3
jak <141954871+jakmac123@users.noreply.github.com>
2023-09-23 22:16:28 +0100

    day one challenges

---
+++ introduction.js
@@ -0,0 +1,10 @@
+// Day 1 Tasks
+
+// comments can make code readable
+
+// Welcome to 30 days of JavaScript
+
+/* comments can make code readable,
+ easy to reuse and informative */
+
+ 
---
+++ variable.js
@@ -0,0 +1,34 @@
+let myName = "Jak";
+let TheGoat = true;
+let myHeight;
+let emptyValue = null
+
+console.log(typeof "Jak");
+console.log(typeof true);
+console.log(null);
+console.log(myHeight);
+
+let favFood;
+let favDrink;
+let favPerson;
+let faveShow;
+
+console.log(favFood, favDrink, favPerson, faveShow);
+
+let favColour = "Red";
+let favNum = 6;
+let favGame = "Fifa";
+let favOutcome = true;
+
+console.log(favColour, favNum, favGame, favOutcome);
+
+let firstName = "Jak", lastName = "Macgregor", Married = false, Country = "England", jakAge = 29;
+
+console.log(firstName, lastName, Married, Country, jakAge);
+
+let myAge = 29;
+let yourAge = 25;
+
+console.log("I am", myAge, "years old.");
+
+console.log("You are", yourAge, "years old.");
